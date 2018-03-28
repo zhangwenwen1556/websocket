2a8e9b1ac4cb2d274593f1e5287e45bef0100e55
张文文 <zhangwenwen1556@163.com>
2018-03-28 18:02:22 +0800

    修改端口

--- src/main/java/com/example/websocket/WebsocketApplication.java
+++ src/main/java/com/example/websocket/WebsocketApplication.java
@@ -2,11 +2,18 @@ package com.example.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 
 @SpringBootApplication
-public class WebsocketApplication {
+public class WebsocketApplication implements EmbeddedServletContainerCustomizer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+		configurableEmbeddedServletContainer.setPort(8000);
 	}
 }
